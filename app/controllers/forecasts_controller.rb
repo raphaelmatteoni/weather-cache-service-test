b54185e2f54e58282fe1e18de1b89007d908e8e7
Raphael Matteoni <raphaelmatteoni@gmail.com>
2026-05-16 15:22:25 -0300

    feat: implement ForecastsController with error handling

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,4 +1,14 @@
 class ForecastsController < ApplicationController
   def index
+    return unless params[:address].present?
+
+    result = WeatherService.call(params[:address])
+    @forecast = result[:data]
+    @cached   = result[:cached]
+    @location = result[:location]
+  rescue GeocodingService::NotFoundError => e
+    flash.now[:alert] = e.message
+  rescue WeatherService::FetchError => e
+    flash.now[:alert] = e.message
   end
 end
